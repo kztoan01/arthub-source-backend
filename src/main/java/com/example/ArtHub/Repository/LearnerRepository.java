b946334f7a707345bba2d28a4e99db2ca97798b1
Gnott-h <phunhse173268@fpt.edu.vn>
2023-09-27 08:44:01 +0700

    recommmit Leaner (Ngo Huynh Tan Loc)

---
+++ src/main/java/com/example/ArtHub/Repository/LearnerRepository.java
@@ -0,0 +1,8 @@
+package com.example.ArtHub.Repository;
+
+import com.example.ArtHub.Entity.Learner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LearnerRepository extends JpaRepository<Learner, Integer> {
+
+}
